934f553ba2f48830cd54c121d065cda54a1dcb0a
SHARIER ISLAM <Sharierpulok475@gmail.com>
2025-05-09 06:54:10 +0600

    Add initial HTML, CSS, and JavaScript files for DOM manipulation demo

---
+++ script.js
@@ -0,0 +1,23 @@
+//DOM Manipulation 
+
+//Selecting with ID
+
+let heading = document.getElementById("header");
+console.dir(heading);
+
+// Selecting with class
+
+let heading4 = document.getElementsByClassName("heading");
+console.dir(heading4);
+console.log(heading4);
+
+//selecting with tag
+
+let para = document.getElementsByTagName("p");
+console.dir(para);
+console.log(para);
+
+
+// Query Selector
+
+//Returns with First Element
